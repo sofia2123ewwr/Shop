7eca78b39674d728992c144f99f7f1a309924a33
Soffia <schoolgirl4991@gmail.com>
2023-01-24 13:03:19 +0200

    log out

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,8 @@ Rails.application.routes.draw do
   get '/cart', to: 'static_pages#cart'
   get '/products', to: 'static_pages#products'
   get '/catalog', to: 'static_pages#catalog'
+  # get '/users',   to: 'users#index'
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   
   # Defines the root path route ("/")
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+User.create!(first_name: "Sofiia", last_name: "Shkilna", phone: "380665154604", email: "schoolgirl4991@gmail.com", password: "sofiiaa", password_confirmation: "sofiiaa")
+# Generate a bunch of additional users.
+99.times do |n|
+    first_name = Faker::Name.first_name 
+    last_name = Faker::Name.last_name
+    phone = Faker::PhoneNumber.phone_number
+    email = "example-#{n+1}@gmail.com"
+    password = "password"
+    User.create!(first_name: first_name, last_name: last_name, email: email, password: password, password_confirmation: password)
+end
